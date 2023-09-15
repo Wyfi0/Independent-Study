1b376d3e11f4432393aea261342277f9853a42b9
Wyfi0 <wyfi08g@gmail.com>
2023-09-14 22:24:11 -0400

    fix2

--- bash/arp-scan-to-file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the name of the file to current date
-out_file="$(date +%D-%R)"
-
-# Find the script's directory using the pwd command
-working_dir="$(pwd)"
-
-# Make the file according to the out_file and say hello!
