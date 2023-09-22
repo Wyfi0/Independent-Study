0ef04999c6cab90e85a738b6e3401c092f3bdeb3
Wyfi0 <wyfi08g@gmail.com>
2023-09-22 14:43:00 -0400

    added nmap functionality to arpscan.sh

--- bash/arpscan.sh
+++ bash/arpscan.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# pass the local subnet to scan
+ip = $1
+
 # Set the name of the file to current date
-out_file="$(date +%D-%R)"
+out_file="$(date +%m-%d-%y)"
 
 # Find the script's directory using the pwd command
 working_dir="$(pwd)"
 
-# Make the file according to the out_file and say hello!
+# Make the file according to the out_file 
 touch "$working_dir/$out_file"
+
+nmap $1 > "$working_dir/$out_file"
