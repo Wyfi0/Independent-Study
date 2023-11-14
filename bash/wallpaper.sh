a8f907c1713cf2d8a693aac1e980179b75669bea
Wyfi0 <wyfi08g@gmail.com>
2023-11-14 11:47:45 -0500

    Started c adventure!

--- bash/wallpaper.sh
+++ bash/wallpaper.sh
@@ -9,16 +9,30 @@ wallpapers=($(ls $1 | awk '/[*.jpg, *.png, *.gif, *.webp, *.bmp, *.tiff]/'))
 echo "You said $1"
 echo "These were found in that dir: ${wallpapers[*]}"
 
+good=false
+
 length=${#wallpapers[@]}
-echo "length is $length"
+echo "$length images"
 
-# Initialize swww
-swww init
+# Before running anything else make sure both passed variables are not null
+if [ -z $1 ] || [ -z $2 ]; then
+	good=false
+	echo "You need to pass the directory of your wallpapers and then the amount of seconds you want each wallpaper to be there!"
+else
+	# and tell the while loop that were good to go!
+	swww init
+	good=true
+fi
 
-while true; do
+# Run this continually
+while [ "$good" = true ]; do
+	# For every item in the wallpaper list
 	for paper in "${wallpapers[@]}"; do
+		# Set image to the directory then  the filename
 		swww img "$1$paper"
+		# Print out the directory then the filename
 		echo "$1$paper"
-		sleep 300
+		# Sleep for the second passed variable
+		sleep $2
 	done
 done
